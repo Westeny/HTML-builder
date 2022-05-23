4a4ad5757064396f1471184d98ed417259193afe
Eugene Shmelev <eugeneshmelev@MacBook-Pro-Eugene.local>
2022-05-24 00:19:55 +0300

    feat: add 01 solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+ 
+var stream = new fs.ReadStream("01-read-file/text.txt");
+
+stream.on('readable', function(){
+    var data = stream.read();
+    if(data != null)console.log(String(data));
+});
+ 
+stream.on('end', function(){
+    console.log();
+});
